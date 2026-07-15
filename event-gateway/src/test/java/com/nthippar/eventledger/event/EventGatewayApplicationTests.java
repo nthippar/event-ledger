6c3a376421d18a7212757028c409b9817ea13978
Nagaraju Thipparapu <thipparapunagaraju3@gmail.com>
2026-07-14 23:18:59 -0500

    feat: create event gateway skeleton

---
+++ event-gateway/src/test/java/com/nthippar/eventledger/event/EventGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nthippar.eventledger.event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
